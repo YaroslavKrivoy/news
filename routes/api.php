2b0e12c39c0977eea8428922bee4fffeae105eb9
yk <yk2@ukrosoft.com.ua>
2021-03-29 16:38:41 +0300

    Create api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/news', [App\Http\Controllers\API\NewsController::class, 'index']);
+Route::get('/news/{news}', [App\Http\Controllers\API\NewsController::class, 'show']);
+Route::get('/news/category/{id}', [App\Http\Controllers\API\NewsController::class, 'showByCategoryId']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('categories', App\Http\Controllers\API\CategoryController::class)->middleware('role:root');
+    Route::get('/userinfo', [App\Http\Controllers\API\AuthController::class, 'userInfo']);
+    Route::middleware('role:User|root')->group(function () {
+        Route::post('/news', [App\Http\Controllers\API\NewsController::class, 'store']);
+        Route::patch('/news/{news}', [App\Http\Controllers\API\NewsController::class, 'update']);
+        Route::delete('/news/{news}', [App\Http\Controllers\API\NewsController::class, 'destroy']);
+    });
+});
+
+Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+
